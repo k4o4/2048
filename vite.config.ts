5d763772b8097db0447b52934a7890a719ac3d14
LuckyStar <luckystartf2@gmail.com>
2025-08-12 21:24:56 +0200

    renamed workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/2048/',
   plugins: [react()],
   define: {
     'import.meta.env.VITE_E2E': JSON.stringify(process.env.VITE_E2E ?? '0')
